9f0b97fc753b568b21b5d0400972df0fe99db27a
leonardo <leonardohenri2015@outlook.com>
2022-12-28 12:21:33 -0300

    initial

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,26 @@
+import imagem from "../../images/image-product-desktop.jpg"
+import "./styles.css"
+
+const valor = 169.99;
+const valordesc = 149.99;
+export const PostCard = () =>{
+    return(
+        <div className="container">
+            <div className="grid">
+                <div>
+                    <img src={imagem} alt="perfume Gabrielle" className="photo"/>
+                                </div>
+                                <div className="text">
+                    <h3>perfume</h3>
+                    <h2>Gabrielle Essence Eau De Parfum</h2>
+                    <p className="desc">A floral, solar and voluptuous interpretation composed by olivier polge , perfumer-creator for the House of CHANEL</p>
+                     <div className="valor">
+                         <p className="valordesc">${valordesc}</p>
+                         <p>${valor}</p>
+                     </div>
+                    <button className="button">add to cart</button>
+                                </div>
+                </div>
+        </div>
+    );
+}
---
+++ src/templates/index.jsx
@@ -0,0 +1,9 @@
+import { PostCard } from '../components/Post';
+import './styles.css'
+
+export const Home = () =>{
+  return(<div className='body'>
+   <PostCard/>
+   </div>
+  );
+}
